0b463a439206c1bf2e4c221bf3d618d865da8cd3
zhang.xiaofei10 <zhang.xiaofei10@zte.com.cn>
2016-09-06 14:19:29 +0800

    running ok;

--- app/hero-search.component.ts
+++ app/hero-search.component.ts
@@ -48,3 +48,8 @@ export class HeroSearchComponent implements OnInit {
 }
 
 
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -56,3 +56,8 @@ export class HeroesComponent implements OnInit {
 }
 
 
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
